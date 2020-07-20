867e0ca03b5ce2e0ac10606a3619f243a94cb777
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-07-20 14:53:23 +0200

    SimpleArray

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,16 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] waitingForGames = new String[5];
+        waitingForGames[0] = "Baldur's Gate 3";
+        waitingForGames[1] = "Assassin's Creed Valhalla";
+        waitingForGames[2] = "CyberPunk 2077";
+        waitingForGames[3] = "Horizon Forbidden West";
+        waitingForGames[4] = "The Waylanders";
+
+        String fourthGame = waitingForGames[3];
+        System.out.println("Czwarty element tablicy: " + fourthGame);
+
+        System.out.println("Moja tablica zawiera " + waitingForGames.length + " elementów.");
+
+    }
+}
