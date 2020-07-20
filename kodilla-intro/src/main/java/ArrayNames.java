de0d39064bbe51cd2130166d336eb48dd48726f9
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-07-20 15:41:24 +0200

    Loops

---
+++ kodilla-intro/src/main/java/ArrayNames.java
@@ -0,0 +1,18 @@
+public class ArrayNames {
+    public static void main(String [] args) {
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+
+        for(int i=0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Teraz od końca...");
+        System.out.println(" ");
+
+        for(int i= names.length -1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+
+    }
+}
---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,24 @@
+public class Loops {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = 23;
+        numbers[1] = 9;
+        numbers[2] = 44;
+        numbers[3] = 5;
+        numbers[4] = 17;
+
+        int sum = 0;
+
+        for(int i=0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("Suma liczb w tablicy: " + sum);
+        System.out.println(" ");
+
+
+
+    }
+}
