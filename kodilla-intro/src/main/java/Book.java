df18038a9fa9ee0932d0542651d1c2018e34f07a
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-08-17 14:55:26 +0200

    2.7 Book

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,21 @@
+
+
+public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title){
+
+    }
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+
+    }
+
+    public static void main(String[] args) {
+        Book theHobbit = Book.of("J.R.R. Tolkien", "The Hobbit");
+        System.out.println(theHobbit);
+
+    }
+}
---
+++ kodilla-intro/src/main/java/Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculator {
+    static final double PI = 3.1415927;
+    static int counter = 0;
+
+    public Calculator() {
+        counter++;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double sub(double a, double b) {
+        return a - b;
+    }
+    public double circleArea(double r) {
+        return PI * r * r;
+    }
+    public double circlePerimeter(double r) {
+        String name = UserDialogs.getUsername();
+        System.out.println("Hi, " + name + ". Thank you for using our software.");
+        return PI * r * 2;
+    }
+}
