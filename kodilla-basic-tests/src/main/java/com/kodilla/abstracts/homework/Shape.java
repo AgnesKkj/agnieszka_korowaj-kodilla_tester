6e8d100d3f574c2b9cc50186059cafac4b6d63de
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-08-22 16:36:24 +0200

    3.2.1. Shapes

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Shape.java
@@ -0,0 +1,10 @@
+package com.kodilla.abstracts.homework;
+
+public abstract class Shape {
+
+    private double area;
+    private double perimeter;
+
+    public abstract double calcArea();
+    public abstract double calcPerimeter();
+}
