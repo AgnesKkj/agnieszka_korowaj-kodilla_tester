643cba364e9b49c4d2267e9415f93f786734f473
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-08-24 15:34:04 +0200

    3.6 CalculatorTestSuite

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 35;
+        int b = 24;
+        int subResult = calculator.sub(a,b);
+        assertEquals(11, subResult);
+    }
+
+    @Test public void testSquareNeg() {
+        Calculator calculator = new Calculator();
+        int a = -5;
+        int squareResultNeg = calculator.square(a);
+        assertEquals(25, squareResultNeg);
+    }
+
+    @Test public void testSquareZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int squareResultZ = calculator.square(a);
+        assertEquals(0, squareResultZ);
+    }
+
+    @Test public void testSquarePos() {
+        Calculator calculator = new Calculator();
+        //ustawione, żeby wyszedł fail
+        int a = 7;
+        int squareResultPos = calculator.square(a);
+        assertEquals(36, squareResultPos);
+    }
+}
