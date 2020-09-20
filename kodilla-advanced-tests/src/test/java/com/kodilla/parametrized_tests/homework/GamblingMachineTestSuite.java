909fe50577ae94b8d799d44b375d799c5dfee802
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-09-20 14:16:49 +0200

    7.2 po poprawkach

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -3,14 +3,14 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTestSuite {
 
@@ -21,12 +21,13 @@ class GamblingMachineTestSuite {
     @CsvFileSource(resources = "/randomNumbersToFail.csv")
     public void shouldThrowExceptionWithInvalidNumbersProvided(String numbers) {
         String[] expectedArray = numbers.split(",");
-        Set<Integer> expectedParsed = new HashSet<>();
         Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
-        expectedSet
+        List<Integer> numbersToValidate = expectedSet
                 .stream()
                 .map(u -> Integer.parseInt(u))
-                .collect(Collectors.toCollection(() -> expectedParsed));
+                .collect(Collectors.toList());
+        Set<Integer> expectedParsed = new HashSet<>(numbersToValidate);
+        System.out.println("Data to throw exception on validateNumbers(): " + expectedParsed);
         Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.validateNumbers(expectedParsed));
     }
 
@@ -35,13 +36,15 @@ class GamblingMachineTestSuite {
     @CsvFileSource(resources = "/randomNumbersToPass.csv")
     public void shouldPassValidatingMethodWithValidNumbersProvided(String numbers) throws InvalidNumbersException {
         String[] expectedArray = numbers.split(",");
-        Set<Integer> expectedParsed = new HashSet<>();
         Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
-        expectedSet
+        List<Integer> numbersToValidate = expectedSet
                 .stream()
                 .map(u -> Integer.parseInt(u))
-                .collect(Collectors.toCollection(() -> expectedParsed));
+                .collect(Collectors.toList());
+        Set<Integer> expectedParsed = new HashSet<>(numbersToValidate);
+        System.out.println("Data to pass validateNumbers(): " + expectedParsed);
         GamblingMachine.validateNumbers(expectedParsed);
+        assertNotEquals(0,expectedParsed.size());
         assertEquals(6,expectedParsed.size());
     }
 
@@ -49,12 +52,13 @@ class GamblingMachineTestSuite {
     @CsvFileSource(resources = "/randomNumbersToFail.csv")
     public void shouldNotCountWinsWithInvalidNumbersProvided(String numbers) throws InvalidNumbersException {
         String[] expectedArray = numbers.split(",");
-        Set<Integer> expectedParsed = new HashSet<>();
         Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
-        expectedSet
+        List<Integer> numbersToValidate = expectedSet
                 .stream()
                 .map(u -> Integer.parseInt(u))
-                .collect(Collectors.toCollection(() -> expectedParsed));
+                .collect(Collectors.toList());
+        Set<Integer> expectedParsed = new HashSet<>(numbersToValidate);
+        System.out.println("Data to throw exception on howManyWins(): " + expectedParsed);
         Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(expectedParsed));
     }
 
@@ -62,12 +66,13 @@ class GamblingMachineTestSuite {
     @CsvFileSource(resources = "/randomNumbersToPass.csv")
     public void shouldCountWinsWithValidNumbersProvided(String numbers) throws InvalidNumbersException {
         String[] expectedArray = numbers.split(",");
-        Set<Integer> expectedParsed = new HashSet<>();
         Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
-        expectedSet
+        List<Integer> numbersToValidate = expectedSet
                 .stream()
                 .map(u -> Integer.parseInt(u))
-                .collect(Collectors.toCollection(() -> expectedParsed));
+                .collect(Collectors.toList());
+        Set<Integer> expectedParsed = new HashSet<>(numbersToValidate);
+        System.out.println("Data to pass howManyWins(): " + expectedParsed);
         System.out.println("Number of wins: " + gamblingMachine.howManyWins(expectedParsed));
         assertNotNull(gamblingMachine.howManyWins(expectedParsed));
     }
