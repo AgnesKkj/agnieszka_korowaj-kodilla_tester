bc4d54972f9009cd0c60b6db0842eeca4eb4c862
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-10-11 16:22:13 +0200

    10.4 without Mvc

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
@@ -13,13 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BookControllerTestSuite {
 
+    BookService bookServiceMock = Mockito.mock(BookService.class);
+    BookController bookController = new BookController(bookServiceMock);
+    List<BookDto> booksList = new ArrayList<>();
+
     @Test
     public void shouldFetchBooks() {
-
         //given
-        BookService bookServiceMock = Mockito.mock(BookService.class);
-        BookController bookController = new BookController(bookServiceMock);
-        List<BookDto> booksList = new ArrayList<>();
         booksList.add(new BookDto("title 1", "author 1"));
         booksList.add(new BookDto("title 2", "author 2"));
         Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
@@ -31,5 +31,37 @@ class BookControllerTestSuite {
         assertThat(result).hasSize(2);
     }
 
+    @Test
+    public void shouldAddBooksUsingAddBookMethodFromController() {
+        //given
+        Mockito.when(bookController.getBooks()).thenReturn(booksList);
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
+        bookController.addBook(new BookDto("title 1", "author 1"));
+        bookController.addBook(new BookDto("title 2", "author 2"));
+        //when
+        List<BookDto> result = bookController.getBooks();
+        System.out.println(bookController.getBooks());
+        //then
+        Mockito.verify(bookServiceMock).addBook(new BookDto("title 1", "author 1"));
+        Mockito.verify(bookServiceMock).addBook(new BookDto("title 2", "author 2"));
+    }
+
+    @Test
+    public void shouldIncreaseListSizeWHenUsingAddBookMethodFromController() {
+        //given
+        Mockito.when(bookController.getBooks()).thenReturn(booksList);
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
+        bookController.addBook(new BookDto("title 1", "author 1"));
+        bookController.addBook(new BookDto("title 2", "author 2"));
+        //when
+        List<BookDto> result = bookController.getBooks();
+        System.out.println(bookController.getBooks());
+        //then
+        assertThat(result).hasSize(2);
+    }
 
 }
