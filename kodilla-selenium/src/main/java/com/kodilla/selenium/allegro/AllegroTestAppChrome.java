192199f0f51cd47729e2783171d20045a9019cae
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-11-01 19:06:40 +0100

    13.5 CSS Selectors

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestAppChrome.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestAppChrome.java
@@ -9,29 +9,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class AllegroTestAppChrome {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "D:/Kodilla/Selenium/selenium-drivers/Chrome/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
 
         //podana w teorii opcja z Alertem z jakiegoś powodu nie działa, więc klikam przycisk w popup po ścieżce absolutnej
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]")));
-        WebElement gdprPopup = driver.findElement(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dialog-content")));
+        WebElement gdprPopup = driver.findElement(By.cssSelector("button[data-role='accept-consent']"));
         gdprPopup.click();
 
-        WebElement inputFieldSearch = driver.findElement(
-                By.xpath("//*[@class=\"mp4t_8 mp4t_0_m mryx_8 mryx_0_s m7er_k4 _fee54_O2svT\"]/div/div/form/input"));
-        inputFieldSearch.sendKeys("Mavic mini");
+        WebElement searchField = driver.findElement(By.cssSelector("div[data-box-name='allegro.metrumHeader.search'] > div > form > input"));
+        searchField.sendKeys("Mavic mini");
 
-        WebElement categoryCombo = driver.findElement(
-                By.xpath("//*[@class=\"mp7g_oh mr3m_1 _r65gb\"]/select"));
+        WebElement categoryCombo = driver.findElement(By.cssSelector("div[data-role='filters'] > div > select"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByValue("/kategoria/elektronika");
+        searchField.submit();
 
-        inputFieldSearch.submit();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div[data-box-name='items-v3'] > div >div > section > article")));
+        List<WebElement> searchResults = driver.findElements(By.cssSelector("div[data-box-name='items-v3'] > div >div > section > article"));
+        System.out.println(searchResults.get(15).getText());
 
     }
 }
