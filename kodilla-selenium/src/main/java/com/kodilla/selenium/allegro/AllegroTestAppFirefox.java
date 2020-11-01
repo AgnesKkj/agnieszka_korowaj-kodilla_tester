9a0ea5abe07ede350a383868783144e1776d5c6a
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-11-01 17:47:47 +0100

    13.4 XPath

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestAppFirefox.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestAppFirefox.java
@@ -19,8 +19,8 @@ public class AllegroTestAppFirefox {
 
         //podana w teorii opcja z Alertem z jakiegoś powodu nie działa, więc klikam przycisk w popup po ścieżce absolutnej
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]")));
-        WebElement inputField = driver.findElement(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]"));
-        inputField.click();
+        WebElement gdprPopup = driver.findElement(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]"));
+        gdprPopup.click();
 
         WebElement inputFieldSearch = driver.findElement(
                 By.xpath("//*[@class=\"mp4t_8 mp4t_0_m mryx_8 mryx_0_s m7er_k4 _fee54_O2svT\"]/div/div/form/input"));
