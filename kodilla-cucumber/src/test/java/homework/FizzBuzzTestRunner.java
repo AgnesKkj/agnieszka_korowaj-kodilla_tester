a8279f5b590261128d71a90b45c9c3c658832a4c
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-11-07 20:32:28 +0100

    14.2 Gherkin

--- kodilla-cucumber/src/test/java/homework/FizzBuzzTestRunner.java
+++ kodilla-cucumber/src/test/java/homework/FizzBuzzTestRunner.java
@@ -1,11 +1,14 @@
+package homework;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources"}
+        features = {"src/test/resources/fizzBuzz"}
 )
 
-public class TestRunner {
+public class FizzBuzzTestRunner {
+
 }
