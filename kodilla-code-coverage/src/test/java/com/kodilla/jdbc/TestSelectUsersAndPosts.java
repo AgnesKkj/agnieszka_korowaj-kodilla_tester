a9676765d334d4682c5fbadef957b905aefddbd0
Agnieszka Korowaj <a.korowaj@gmail.com>
2020-11-14 18:13:57 +0100

    15.3 JDBC

--- kodilla-code-coverage/src/test/java/com/kodilla/jdbc/TestSelectUsersAndPosts.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/TestSelectUsersAndPosts.java
@@ -1,7 +1,7 @@
 package com.kodilla.jdbc;
 
-import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.ResultSet;
@@ -10,26 +10,47 @@ import java.sql.Statement;
 
 public class TestSelectUsersAndPosts {
 
+    @Before
+    public void cleanDataBaseRecords() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+        Statement statement = dbManager.getConnection().createStatement();
+
+        String deleteQuery = "DELETE FROM POSTS\n" +
+                "WHERE ID = 20;";
+        statement.executeUpdate(deleteQuery);
+        deleteQuery = "DELETE FROM POSTS\n" +
+                "WHERE ID = 21;";
+        statement.executeUpdate(deleteQuery);
+        deleteQuery = "DELETE FROM POSTS\n" +
+                "WHERE ID = 22;";
+        statement.executeUpdate(deleteQuery);
+    }
+
     @Test
     public void testSelectUsersAndPosts() throws SQLException {
         //Given
         DbManager dbManager = DbManager.getInstance();
-        String countQuery = "SELECT COUNT(*) FROM USERS";
         Statement statement = dbManager.getConnection().createStatement();
+
+        String countQuery = "SELECT COUNT(DISTINCT U.ID) \n" +
+                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "HAVING COUNT(P.BODY) > 2;";
+
         ResultSet rs = statement.executeQuery(countQuery);
         int count = 0;
         while (rs.next()) {
-            count = rs.getInt("COUNT(*)");
+            count = rs.getInt("COUNT(DISTINCT U.ID)");
         }
-        String addQuery = "insert into posts(id,user_id,body) values (20,2,\"I burned my cookies.\")";
+        System.out.println("Initial count: " + count);
+
+        String addQuery = "INSERT INTO POSTS(ID,USER_ID,BODY) VALUES (20,2,'I burned my cookies.')";
         statement.executeUpdate(addQuery);
-        String addQueryTwo = "insert into posts(id,user_id,body) values (21,2,\"Now shops are closed and I can't buy any.\")";
-        statement.executeUpdate(addQueryTwo);
-        String addQueryThree = "insert into posts(id,user_id,body) values (22,2,\"Shame.\")";
-        statement.executeUpdate(addQueryThree);
+        addQuery = "INSERT INTO POSTS(ID,USER_ID,BODY) VALUES (21,2,'Now shops are closed and I cannot buy any.')";
+        statement.executeUpdate(addQuery);
+        addQuery = "INSERT INTO POSTS(ID,USER_ID,BODY) VALUES (22,2,'Shame.')";
+        statement.executeUpdate(addQuery);
 
         //When
-
         String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(P.BODY) AS POST_NUMBER\n" +
                 "FROM USERS U\n" +
                 "JOIN POSTS P ON U.ID = P.USER_ID\n" +
@@ -42,29 +63,19 @@ public class TestSelectUsersAndPosts {
 
         int counter = 0;
         while(rs.next()) {
-            System.out.println(rs.getInt("ID") + ", " +
-                    rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
+            System.out.println("End query names: " + rs.getString("U.FIRSTNAME") + ", " +
+                    rs.getString("U.LASTNAME"));
             counter++;
         }
+        System.out.println("End query count: " + counter);
+
         int expected = count + 1;
         Assert.assertEquals(expected, counter);
 
         rs.close();
         statement.close();
+
     }
 
-    @After
-    public void removeEntries() throws SQLException {
-        DbManager dbManager = DbManager.getInstance();
-        Statement statement = dbManager.getConnection().createStatement();
-        String deleteQuery = "delete from posts\n" +
-                "where id = 20;\n" +
-                "delete from posts\n" +
-                "where id = 21;\n" +
-                "delete from posts\n" +
-                "where id = 22;\n";
-        statement.executeUpdate(deleteQuery);
-    }
 
 }
